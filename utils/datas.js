bf7fb5b4112563ded88c43a0c76c058356efa0d7
快乐编程人 <happycoding996@gmail.com>
2023-08-03 15:26:32 +0800

    Update datas.js

--- utils/datas.js
+++ utils/datas.js
@@ -1,18 +1,16 @@
 export default {
 	//用户状态
-	user_status_json: [{'value': 1, 'text': '正常'}, {'value': 0, 'text': '禁用'}],
+	user_status_json: [{'value': "1", 'text': '正常'}, {'value': "0", 'text': '禁用'}],
 	//用户认证
-	user_certified_json: [{'value': 0, 'text': '未认证'}, {'value': 1, 'text': '已认证'}, {'value': 2, 'text': '审核中'}, {'value': 3, 'text': '认证失败'}],
+	user_certified_json: [{'value': "0", 'text': '未认证'}, {'value': "1", 'text': '已认证'}, {'value': "2", 'text': '审核中'}, {'value': "3", 'text': '认证失败'}],
 	//任务类型
 	task_type_json: [{'value': 3, 'text': '间隔任务'}, {'value': 2, 'text': 'cron任务'}],
 	//文件类型
 	file_type_json: [{'value': 2, 'text': '图片'}, {'value': 3, 'text': '文件'}, {'value': 4, 'text': '语音'}, {'value': 5, 'text': '视频'}],
 	//文件状态
 	file_status_json: [{'value': 1, 'text': '临时'}, {'value': 2, 'text': '永存'}],
-	//菜单状态
-	menu_status_json: [{'value': 1, 'text': '启用'}, {'value': 0, 'text': '禁用'}],
 	//是否状态
-	if_status_json: [{'value': 1, 'text': '是'}, {'value': 0, 'text': '否'}],
+	if_status_json: [{'value': "1", 'text': '是'}, {'value': "0", 'text': '否'}],
 	//字段类型
 	field_type_json: [
 		{'value': 1, 'text': 'String'},
